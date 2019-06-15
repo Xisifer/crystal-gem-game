52085269d17147669cdbd5feb30aaf6eb463deb7
Scott Finlay <xisifer@gmail.com>
2019-06-15 10:03:19 -0500

    Finished styling the HTML/CSS. PSeudocoded out an outline of what needs to be done. Gotta start somewhere, right?

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+var redgem;
+var greengem;
+var yellowgem;
+var purplegem;
+
+$(".gembutton").click(
+
+);
+
+
+
+var TargetScore
+
+// When the game is started, several things need to happen:
+//    1. A random number is generated as the Target Score (between 19 - 120)
+
+
+    // 2. This random number is shown to the player
+    // 3. Random Value is chosen for each gem and assigned to that button (between 1 - 12)
+        // a. Garnet
+        // b. Amethyst
+        // c. Pearl
+        // d. Diamond
+    // 4. The number of Games Won is set
+    // 5. The number of Games Lost is set
+
+    // When the user clicks on a gem, several things happen:
+        // 1. That gem's value is added to the Current Score
+        // 2. The Current Score is updated
+
+    
+    // If the user's score goes above the Target Score, the user loses.
+        // Game is reset (via Function!)
+        // Losses is increased by 1
+    // If the user's score matches the Target Score, the user wins!!
+        // Game is reset (via Function!)
+        // Wins is increased by 1
+
+
+    // RESET!! (This should all be wrapped in a Function)
+        // New random Target Score is generated and assigned
+        // New random Gem values are generated and assigned
+        // Target Score on the screen is updated on the screen
+        // User's Current Score is reset back to 0
+        // User's Current Score is updated on the screen
